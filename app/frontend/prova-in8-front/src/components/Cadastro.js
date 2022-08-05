1598deab0194a076ca508fe2e0bb3dd13cd354a0
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-08-04 22:02:27 -0300

    feat: 🎸 Inicio da aplicação Front

--- app/frontend/prova-in8-front/src/components/Cadastro.js
+++ app/frontend/prova-in8-front/src/components/Cadastro.js
@@ -4,25 +4,27 @@ import '../style/Cadastro.css'
 function Cadastro() {
   return(
     <div className="main-cadastro">
-      <header className="header-cadastro">
-        <h2 className="h2-cadastro">CADASTRO</h2>
-      </header>
-      <form className="form-cadastro">
-        <label className="label-cadastro" htmlFor="input-nome"> Nome
-          <input className="input-cadastro" id="input-nome" type='text' />
-        </label>
-        <label className="label-cadastro"> E-mail
-          <input className="input-cadastro" id="input-email" type='text' />
-        </label>
-        <label className="label-cadastro"> Nascimento
-          <input className="input-cadastro" id="input-nascimento" type='date' />
-        </label>
-        <label className="label-cadastro"> Telefone
-          <input className="input-cadastro" id="input-telefone" type='tel' />
-        </label>
-      </form>
-      <div className="button-div">
-        <button type="button" className="button-cadastro">CADASTRAR</button>
+      <div className="container-cadastro">
+        <header className="header-cadastro">
+          <p className="h2-cadastro">CADASTRO</p>
+        </header>
+        <form className="form-cadastro">
+          <label className="label-cadastro" htmlFor="input-nome"> Nome
+            <input className="input-cadastro" id="input-nome" type='text' required />
+          </label>
+          <label className="label-cadastro"> E-mail
+            <input className="input-cadastro" id="input-email" type='text' placeholder="email@email.com" required />
+          </label>
+          <label className="label-cadastro"> Nascimento
+            <input type="text" className="input-cadastro date-mask" placeholder="dd/mm/aaaa" data-mask="00/00/0000" maxlength="10" autocomplete="off" required/>
+          </label>
+          <label className="label-cadastro"> Telefone
+            <input className="cel-sp-mask input-cadastro" id="input-telefone" type="text" placeholder="(00) 00000-0000" required />
+          </label>
+        </form>
+        <div className="button-div">
+          <button type="button" className="btn btn-primary btn-lg button-cadastro">CADASTRAR</button>
+        </div>
       </div>
     </div>
   )
